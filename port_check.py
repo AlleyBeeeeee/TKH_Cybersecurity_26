4d65f3cf0287b6febe1ba569bf5424dc705a2b9c
AlleyBeeeeee <alexandra.blandon@gmail.com>
2026-03-24 00:06:59 +0000

    Week 3 | Session 07 | Port Checker Built

---
+++ port_check.py
@@ -0,0 +1,25 @@
+import socket
+
+#List of Servers
+targets = ["127.0.0.1", "8.8.8.8", "1.1.1.1", "10.0.0.1"]
+
+for ip in targets:
+	print(f"---Checking Server: {ip} ---")
+
+#1 . Create the hand
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#2. Set the timer
+	s.settimeout(1)
+
+#3. Knock on Port 22 (SSH)
+	result = s.connect_ex((ip, 22))
+
+#4 Interpret the result: 0 mean  "Open" anything else means 'closed'
+	if result == 0:
+		print(f"Success: Port 22  is OPEN on {ip}")
+	else:
+		print(f"Failed: Port 22 is CLOSED on {ip}")
+
+# close connection
+	s.close()
